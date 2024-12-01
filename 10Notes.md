bda94c03500aef970cb763d2588dfc1ea78fed70
Tejas Bobade <tejasbobade2002@gmail.com>
2024-12-01 10:19:56 +0530

    Todo App code

--- 10Notes.md
+++ 10Notes.md
@@ -9,4 +9,22 @@
 - Inside it create a file name -> TodoContext.js
 - create index.js to export TodoContext
 
+### Create a function in app
+- add all the function in app.jsx ex. add , update, delete etc.
+- hook used are useState
+- also import TodoProvider
 // 40.10
+
+### Local Storage
+- Window localStorage
+- Set and retrieve localStorage name/value pair:
+localStorage.setItem("lastname", "Smith");
+localStorage.getItem("lastname");
+- Description
+The localStorage object allows you to save key/value pairs in the browser.
+- Note
+The localStorage object stores data with no expiration date.
+The data is not deleted when the browser is closed, and are available for future sessions.
+
+2. Local Storage setup
+- useEffect to get todos
--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { TodoProvider } from "./contexts/TodoContext"
+// import './App.css'
+import TodoForm from "./components/TodoFrom"
+import TodoItem from "./components/TodoItem"
 
 function App() {
   const [todos , setTodos] = useState([])
@@ -17,20 +20,43 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo))
   }
+
+  // We need the hook which brings us all the values of todos when page load
+  useEffect(() => {
+    // localStorage geve values in string we have to convere it into json
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    // we have to set values now add condition check if we don't have that value add we set it application will crash
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  },[]) // Now we need to add the todos given by user but if we put it in dependencies it also get that item which we don't want
+  // Another method
+
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
 
   return (
-    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+    <TodoProvider value={{todos, addTodo , updateTodo, deleteTodo, toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
               {/* Todo form goes here */} 
+              <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
               {/*Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                <div key={todo.id} 
+                className= 'w-full'
+                >
+                  <TodoItem todo={todo} />
+                </div>
+              ))}
           </div>
       </div>
     </div>
